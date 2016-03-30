7748575e1261aa88d4cfda80270409da4bc33a75
Bazilia <ZhabinaVasilisa@gmail.com>
2016-03-31 02:05:52 +0600

    no message

--- src/select.c
+++ src/select.c
@@ -48,7 +48,7 @@ void doSelect(char* logFileName,char* command,char* arguments){
       if (dup2(fd0[0],0) == -1){
         perror("Не могу заменить дескриптор stdin: ");
         return;
-      }
+      }    
       if (dup2(fd1[1],1) == -1){
         perror("Не могу заменить дескриптор stdout: ");
         return;
@@ -57,7 +57,7 @@ void doSelect(char* logFileName,char* command,char* arguments){
         perror("Не могу заменить дескриптор stderr: ");
         return;
       }
-      char* args[1] = {arguments};
+      char* args[] = {command, arguments, NULL};
       if (execv(command,args) == -1){
         perror("Не могу выполнить команду: ");
         return;
