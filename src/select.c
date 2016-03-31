11611a2a471d2863920f56b551f12c6dd77b9773
Bazilia <ZhabinaVasilisa@gmail.com>
2016-03-31 13:17:53 +0600

    no message

--- src/select.c
+++ src/select.c
@@ -4,130 +4,181 @@
 #include <stdio.h>
 #include <errno.h>
 #include <signal.h>
+#include <string.h>
+#include <time.h>
 
 int gotSignal = 0;
+siginfo_t childInfo;
 void childHandler(int sigNumber, siginfo_t *siginfo, void *context){
-  gotSignal = 1;
+	gotSignal = 1;
+	childInfo = *siginfo;
 }
 
-void doSelect(char* logFileName,char* command,char** arguments){
-  int fd0[2], fd1[2], fd2[2];
-  fd_set fds;
-  printf("Select пошёл\n" );
-  if(pipe(fd0) != 0){
-    perror("Не могу открыть первую пайпу");
-    return;
-  }
-  if(pipe(fd1) != 0){
-    perror("Не могу открыть вторую пайпу");
-    return;
-  }
-  if(pipe(fd2) != 0){
-    perror("Не могу открыть третью пайпу");
-    return;
-  }
-  printf("Пайпы создались \n");
-  pid_t child = fork();
-    if(child == -1){
-      perror("Не могу форкнуться: ");
-    }else if(child == 0){
-      if(close(fd0[1])){
-        perror("Не могу закрыть писателя в потомке на stdin: ");
-        return;
-      }
-      if(close(fd1[0])){
-        perror("Не могу закрыть читателя в потомке на stdout: ");
-        return;
-      }
-      if(close(fd2[0])){
-        perror("Не могу закрыть читателя в потомке на stderr: ");
-        return;
-      }
-      if (dup2(fd0[0],0) == -1){
-        perror("Не могу заменить дескриптор stdin: ");
-        return;
-      }
-      if (dup2(fd1[1],1) == -1){
-        perror("Не могу заменить дескриптор stdout: ");
-        return;
-      }
-      if (dup2(fd2[1],2) == -1){
-        perror("Не могу заменить дескриптор stderr: ");
-        return;
-      }
-      if (execvp  (command,arguments) == -1){
-        perror("Не могу выполнить команду: ");
-        return;
-      }
-      return;
-    }else{
-      struct sigaction act;
-      act.sa_sigaction = &childHandler;
-      act.sa_flags = SA_SIGINFO;
-      sigset_t mask;
-      sigemptyset(&mask);
-      sigaddset(&mask, SIGCHLD);
-      act.sa_mask = mask;
-      if (sigaction(SIGCHLD, &act, NULL) == -1) {
-        perror("Не могу поменять обработчик SIGCHILD: ");
-        return;
-      }
-      if(close(fd0[0])){
-        perror("Не могу закрыть читателя в предке на stdin: ");
-        return;
-      }
-      if(close(fd1[1])){
-        perror("Не могу закрыть писателя в предке на stdout: ");
-        return;
-      }
-      if(close(fd2[1])){
-        perror("Не могу закрыть писателя в предке на stderr: ");
-        return;
-      }
-      while(!gotSignal){
-        struct timeval waitTime;
-        char currentRead[1000];
+void doSelect(char* logFileName, char* command, char** arguments){
+	int fd0[2], fd1[2], fd2[2];
+	fd_set fds;
+	printf("Select пошёл\n");
+	if (pipe(fd0) != 0){
+		perror("Не могу открыть первую пайпу");
+		return;
+	}
+	if (pipe(fd1) != 0){
+		perror("Не могу открыть вторую пайпу");
+		return;
+	}
+	if (pipe(fd2) != 0){
+		perror("Не могу открыть третью пайпу");
+		return;
+	}
+	printf("Пайпы создались \n");
+	pid_t child = fork();
+	if (child == -1){
+		perror("Не могу форкнуться: ");
+	}
+	else if (child == 0){
+		if (close(fd0[1])){
+			perror("Не могу закрыть писателя в потомке на stdin: ");
+			return;
+		}
+		if (close(fd1[0])){
+			perror("Не могу закрыть читателя в потомке на stdout: ");
+			return;
+		}
+		if (close(fd2[0])){
+			perror("Не могу закрыть читателя в потомке на stderr: ");
+			return;
+		}
+		if (dup2(fd0[0], 0) == -1){
+			perror("Не могу заменить дескриптор stdin: ");
+			return;
+		}
+		if (dup2(fd1[1], 1) == -1){
+			perror("Не могу заменить дескриптор stdout: ");
+			return;
+		}
+		if (dup2(fd2[1], 2) == -1){
+			perror("Не могу заменить дескриптор stderr: ");
+			return;
+		}
+		if (execvp(command, arguments) == -1){
+			perror("Не могу выполнить команду: ");
+			return;
+		}
+		return;
+	}
+	else{
+		//--получаем дескриптор файла, по умолчанию ставим stderr--
+		int logFD = 2;
+		if (logFileName != NULL) {
+			logFD = open(logFileName, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+			if (logFD == -1) {
+				perror("Не могу открыть/создать файл: ");
+				return;
+			}
+		}
+		//-------
+		struct sigaction act;
+		act.sa_sigaction = &childHandler;
+		act.sa_flags = SA_SIGINFO;
+		sigset_t mask;
+		sigemptyset(&mask);
+		sigaddset(&mask, SIGCHLD);
+		act.sa_mask = mask;
+		if (sigaction(SIGCHLD, &act, NULL) == -1) {
+			perror("Не могу поменять обработчик SIGCHILD: ");
+			return;
+		}
+		if (close(fd0[0])){
+			perror("Не могу закрыть читателя в предке на stdin: ");
+			return;
+		}
+		if (close(fd1[1])){
+			perror("Не могу закрыть писателя в предке на stdout: ");
+			return;
+		}
+		if (close(fd2[1])){
+			perror("Не могу закрыть писателя в предке на stderr: ");
+			return;
+		}
+		while (!gotSignal){
+			struct timeval waitTime;
+			char currentRead[1000];
 
-        waitTime.tv_sec = 1;
-        waitTime.tv_usec = 0;
+			waitTime.tv_sec = 1;
+			waitTime.tv_usec = 0;
 
-        FD_ZERO(&fds);
-        FD_SET(0,&fds);
-        FD_SET(fd1[0],&fds);
-        FD_SET(fd2[0],&fds);
+			FD_ZERO(&fds);
+			FD_SET(0, &fds);
+			FD_SET(fd1[0], &fds);
+			FD_SET(fd2[0], &fds);
 
-        int res = select(FD_SETSIZE, &fds, NULL, NULL,&waitTime);
-        if(res == -1){
-          perror ("Ошибка в селекте: ");
-          return;
-        }
-        if(FD_ISSET(0,&fds)){
-          int readSize = read(0, currentRead, sizeof(currentRead));
-          if(readSize == -1){
-            perror("Не могу считать из stdin: ");
-            return;
-          }
-          if(strcmp(currentRead,"exit\n")){
-            kill(SIGKILL,child);
-          }
-          write(fd0[1],currentRead,readSize);
-        }
-        if(FD_ISSET(fd1[0],&fds)){
-          int readSize = read(fd1[0], currentRead, sizeof(currentRead));
-          if(readSize == -1){
-            perror("Не могу считать из пайпы для stdout: ");
-            return;
-          }
-          write(1,currentRead,readSize);
-        }
-        if(FD_ISSET(fd2[0],&fds)){
-          int readSize = read(fd2[0], currentRead, sizeof(currentRead));
-          if(readSize == -1){
-            perror("Не могу считать из пайпы для stderr: ");
-            return;
-          }
-          write(2,currentRead,readSize);
-        }
-      }
-    }
+
+
+			int res = select(FD_SETSIZE, &fds, NULL, NULL, &waitTime);
+			if (res == -1){
+				perror("Ошибка в селекте: ");
+				return;
+			}
+			//Получаем текущее время
+			char timeStr[18];
+			time_t t = time(NULL);
+			struct tm tm = *localtime(&t);
+			strftime(timeStr, 18, "%D %H:%M:%S", &tm);
+			if (res == 0) {
+				//--Выводим лог в файл или stderr--
+				write(logFD, timeStr, strlen(timeStr));
+				write(logFD, " / NO IO", 8);
+				//------
+			}
+			if (FD_ISSET(0, &fds)){
+				int readSize = read(0, currentRead, sizeof(currentRead));
+				if (readSize == -1){
+					perror("Не могу считать из stdin: ");
+					return;
+				}
+				if (strcmp(currentRead, "exit", 4) == 0){
+					kill(SIGKILL, child);
+				}
+				write(fd0[1], currentRead, readSize);
+				//--Выводим инфу на экран--
+				printf("%d / >0 / %s\n", child, str);
+				//--Выводим лог в файл или stderr--
+				write(logFD, timeStr, strlen(timeStr));
+				write(logFD, " / >0 / ", 8);
+				write(logFD, str, strlen(str));
+				//------
+			}
+			if (FD_ISSET(fd1[0], &fds)){
+				int readSize = read(fd1[0], currentRead, sizeof(currentRead));
+				if (readSize == -1){
+					perror("Не могу считать из пайпы для stdout: ");
+					return;
+				}
+				//write(fd0[1],currentRead,readSize);
+				//--Выводим инфу на экран--
+				printf("%d / <1 / %s\n", child, str);
+				//--Выводим лог в файл или stderr--
+				write(logFD, timeStr, strlen(timeStr));
+				write(logFD, " / <1 / ", 8);
+				write(logFD, str, strlen(str));
+				//------
+			}
+			if (FD_ISSET(fd2[0], &fds)){
+				int readSize = read(fd2[0], currentRead, sizeof(currentRead));
+				if (readSize == -1){
+					perror("Не могу считать из пайпы для stderr: ");
+					return;
+				}
+				//write(fd0[1],currentRead,readSize);
+				//--Выводим инфу на экран--
+				printf("%d / <2 / %s\n", child, str);
+				//--Выводим лог в файл или stderr--
+				write(logFD, timeStr, strlen(timeStr));
+				write(logFD, " / <2 / ", 8);
+				write(logFD, str, strlen(str));
+				//------
+			}
+		}
+		printf("%d TERMINATED WITH CODE %d", child, childInfo.si_code);
+	}
 }
--- src/signals.c
+++ src/signals.c
@@ -4,114 +4,202 @@
 #include <stdio.h>
 #include <errno.h>
 #include <signal.h>
+#include <string.h>
+#include <fcntl.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <time.h>
 
-int gotChildSignal = 0, gotIoSignal = 0;
+int gotChildSignal = 0, gotInSignal = 0, gotOutSignal = 0, gotErrorSignal = 0;
+int fd0[2], fd1[2], fd2[2];
 siginfo_t info;
+siginfo_t childInfo;
 void helpHandler(int sigNumber, siginfo_t *siginfo, void *context){
-  if (sigNumber == SIGCHLD){
-    gotChildSignal = 1;
-  }
-  if(sigNumber == SIGIO){
-    gotIoSignal = 1;
-    info = *siginfo;
-  }
+	if (sigNumber == SIGCHLD){
+		gotChildSignal = 1;
+		childInfo = *siginfo;
+	}
+	if (sigNumber == SIGIO){
+		info = *siginfo;
+		if (info.si_fd == 0){
+			gotInSignal = 1;
+		}
+		if (info.si_fd == fd1[0]){
+			gotOutSignal = 1;
+		}
+		if (info.si_fd == fd2[0]){
+			gotErrorSignal = 1;
+		}
+	}
 }
 
 void doSignals(char* logFileName, char* command, char** arguments){
-  int fd0[2], fd1[2], fd2[2];
-  printf("Signals пошёл\n" );
-  if(pipe(fd0) != 0){
-    perror("Не могу открыть первую пайпу");
-    return;
-  }
-  if(pipe(fd1) != 0){
-    perror("Не могу открыть вторую пайпу");
-    return;
-  }
-  if(pipe(fd2) != 0){
-    perror("Не могу открыть третью пайпу");
-    return;
-  }
-  printf("Пайпы создались \n");
-  pid_t child = fork();
-    if(child == -1){
-      perror("Не могу форкнуться: ");
-    }else if(child == 0){
-      if(close(fd0[1])){
-        perror("Не могу закрыть писателя в потомке на stdin: ");
-        return;
-      }
-      if(close(fd1[0])){
-        perror("Не могу закрыть читателя в потомке на stdout: ");
-        return;
-      }
-      if(close(fd2[0])){
-        perror("Не могу закрыть читателя в потомке на stderr: ");
-        return;
-      }
-      if (dup2(fd0[0],0) == -1){
-        perror("Не могу заменить дескриптор stdin: ");
-        return;
-      }
-      if (dup2(fd1[1],1) == -1){
-        perror("Не могу заменить дескриптор stdout: ");
-        return;
-      }
-      if (dup2(fd2[1],2) == -1){
-        perror("Не могу заменить дескриптор stderr: ");
-        return;
-      }
-      if (execvp  (command,arguments) == -1){
-        perror("Не могу выполнить команду: ");
-        return;
-      }
-      return;
-    }else{
-      struct sigaction act;
-      act.sa_sigaction = &helpHandler;
-      act.sa_flags = SA_SIGINFO;
-      sigset_t mask;
-      sigemptyset(&mask);
-      sigaddset(&mask, SIGCHLD);
-      sigaddset(&mask, SIGIO);
-      act.sa_mask = mask;
-      if(sigaction(SIGCHLD, &act, NULL) == -1) {
-        perror("Не могу поменять обработчик SIGCHILD: ");
-        return;
-      }
-      if(sigaction(SIGIO, &act, NULL) == -1) {
-        perror("Не могу поменять обработчик SIGCHILD: ");
-        return;
-      }
-      if(close(fd0[0])){
-        perror("Не могу закрыть читателя в предке на stdin: ");
-        return;
-      }
-      if(close(fd1[1])){
-        perror("Не могу закрыть писателя в предке на stdout: ");
-        return;
-      }
-      if(close(fd2[1])){
-        perror("Не могу закрыть писателя в предке на stderr: ");
-        return;
-      }
-      fcntl(0,F_SETFL,O_ASYNC);
-      fcntl(fd1[0],F_SETFL,O_ASYNC);
-      fcntl(fd2[0],F_SETFL,O_ASYNC);
+	printf("Signals пошёл\n");
+	if (pipe(fd0) != 0){
+		perror("Не могу открыть первую пайпу");
+		return;
+	}
+	if (pipe(fd1) != 0){
+		perror("Не могу открыть вторую пайпу");
+		return;
+	}
+	if (pipe(fd2) != 0){
+		perror("Не могу открыть третью пайпу");
+		return;
+	}
+	printf("Пайпы создались \n");
+	pid_t child = fork();
+	if (child == -1){
+		perror("Не могу форкнуться: ");
+	}
+	else if (child == 0){
+		if (close(fd0[1])){
+			perror("Не могу закрыть писателя в потомке на stdin: ");
+			return;
+		}
+		if (close(fd1[0])){
+			perror("Не могу закрыть читателя в потомке на stdout: ");
+			return;
+		}
+		if (close(fd2[0])){
+			perror("Не могу закрыть читателя в потомке на stderr: ");
+			return;
+		}
+		if (dup2(fd0[0], 0) == -1){
+			perror("Не могу заменить дескриптор stdin: ");
+			return;
+		}
+		if (dup2(fd1[1], 1) == -1){
+			perror("Не могу заменить дескриптор stdout: ");
+			return;
+		}
+		if (dup2(fd2[1], 2) == -1){
+			perror("Не могу заменить дескриптор stderr: ");
+			return;
+		}
+		if (execvp(command, arguments) == -1){
+			perror("Не могу выполнить команду: ");
+			return;
+		}
+		return;
+	}
+	else{
+		int logFD = 2;
+		if (logFileName != NULL) {
+			logFD = open(logFileName, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+			if (logFD == -1) {
+				perror("Не могу открыть/создать файл: ");
+				return;
+			}
+		}
 
-      if(fcntl(0,F_SETSIG,SIGIO) == -1){
-        perror();
-      }
-      if(fcntl(fd1[0],F_SETSIG,SIGIO) == -1){
-        perror();
-      }
-      if(fcntl(fd2[0],F_SETSIG,SIGIO) == -1){
-        perror();
-      }
-      while(!gotChildSignal){
-        sleep(1);
-        if(){
+		struct sigaction act;
+		act.sa_sigaction = &helpHandler;
+		act.sa_flags = SA_SIGINFO;
+		sigset_t mask;
+		sigemptyset(&mask);
+		sigaddset(&mask, SIGCHLD);
+		sigaddset(&mask, SIGIO);
+		act.sa_mask = mask;
+		if (sigaction(SIGCHLD, &act, NULL) == -1) {
+			perror("Не могу поменять обработчик SIGCHILD: ");
+			return;
+		}
+		if (sigaction(SIGIO, &act, NULL) == -1) {
+			perror("Не могу поменять обработчик SIGCHILD: ");
+			return;
+		}
+		if (close(fd0[0])){
+			perror("Не могу закрыть читателя в предке на stdin: ");
+			return;
+		}
+		if (close(fd1[1])){
+			perror("Не могу закрыть писателя в предке на stdout: ");
+			return;
+		}
+		if (close(fd2[1])){
+			perror("Не могу закрыть писателя в предке на stderr: ");
+			return;
+		}
+		fcntl(0, F_SETFL, O_ASYNC);
+		fcntl(fd1[0], F_SETFL, O_ASYNC);
+		fcntl(fd2[0], F_SETFL, O_ASYNC);
 
-        }
-      }
+		if (fcntl(0, F_SETSIG, SIGIO) == -1){
+			perror("Не могу повесить сигнал на stdin: ");
+			return;
+		}
+		fcntl(0, F_SETOWN, getpid());
+		if (fcntl(fd1[0], F_SETSIG, SIGIO) == -1){
+			perror("Не могу повесить сигнал на чтение из пайпы: ");
+			return;
+		}
+		fcntl(fd1[0], F_SETOWN, getpid());
+		if (fcntl(fd2[0], F_SETSIG, SIGIO) == -1){
+			perror("Не могу повесить сигнал на чтение ошибок из пайпы:");
+			return;
+		}
+		fcntl(fd2[0], F_SETOWN, getpid());
+
+		while (!gotChildSignal){
+			char currentRead[1000];
+			sleep(1);
+			char timeStr[18];
+			time_t t = time(NULL);
+			struct tm tm = *localtime(&t);
+			strftime(timeStr, 18, "%D %H:%M:%S", &tm);
+			if (gotInSignal == 1){
+				gotInSignal = 0;
+				int readSize = read(0, currentRead, sizeof(currentRead));
+				if (readSize == -1){
+					perror("Не могу считать из stdin: ");
+					return;
+				}
+				if (strcmp(currentRead, "exit\n")){
+					kill(SIGKILL, child);
+				}
+				write(fd0[1], currentRead, readSize);
+
+				printf("%d / >0 / %s\n", child, str);
+
+				write(logFD, timeStr, strlen(timeStr));
+				write(logFD, " / >0 / ", 8);
+				write(logFD, str, strlen(str));
+			}
+			else if (gotOutSignal == 1){
+				gotOutSignal = 0;
+
+				int readSize = read(fd1[0], currentRead, sizeof(currentRead));
+				if (readSize == -1){
+					perror("Не могу считать из пайпы для stdout: ");
+					return;
+				}
+				printf("%d / <1 / %s\n", child, str);
+
+				write(logFD, timeStr, strlen(timeStr));
+				write(logFD, " / <1 / ", 8);
+				write(logFD, str, strlen(str));
+			}
+			else if (gotErrorSignal == 1){
+				gotErrorSignal = 0;
+
+				int readSize = read(fd2[0], currentRead, sizeof(currentRead));
+				if (readSize == -1){
+					perror("Не могу считать из пайпы для stderr: ");
+					return;
+				}
+				printf("%d / <2 / %s\n", child, str);
+
+				write(logFD, timeStr, strlen(timeStr));
+				write(logFD, " / <2 / ", 8);
+				write(logFD, str, strlen(str));
+			}
+			else{
+				write(logFD, timeStr, strlen(timeStr));
+				write(logFD, " / NO IO", 8);
+			}
+		}
+		printf("%d TERMINATED WITH CODE %d", child, childInfo.si_code);
+	}
+
 }
